db4bc6b64890e8922fdd1364220733cf021e2551
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-25 15:19:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,15 @@
 require "yaml"
-def load_library(library)
-  emotions = YAML.load_file(library)
-  return emotions
-end 
+  def load_library(lib)
+   emotions = YAML.load_file(lib)
+   emotions.each do |key, emo|
+   emot[key] = {
+    :english => emo[0],
+    :japanese => emo[1]
+  }
+  end
+  return emot
+end
+
 
 def get_japanese_emoticon
   # code goes here
