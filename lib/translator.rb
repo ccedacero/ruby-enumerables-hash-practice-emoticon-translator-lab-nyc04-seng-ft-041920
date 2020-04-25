7bb6f47b8a5d328853a7ac84bbdff54317463383
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-25 15:17:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
-# require modules here
-
-def load_library
-  # code goes here
-end
+require "yaml"
+def load_library(library)
+  emotions = YAML.load_file(library)
+  return emotions
+end 
 
 def get_japanese_emoticon
   # code goes here
