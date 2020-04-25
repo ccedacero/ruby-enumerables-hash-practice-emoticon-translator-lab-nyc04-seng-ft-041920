fbce4f1b2bf398e28be1715afb38a0885be622ef
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-25 15:41:31 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,34 @@
-# require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+  def load_library(lib)
+   emotions = YAML.load_file(lib)
+   emoticon = {}
+   emotions.each do |key, emo|
+   emoticon[key] = {
+    :english => emo[0],
+    :japanese => emo[1]
+  }
+  end
+  return emoticon
 end
 
-def get_japanese_emoticon
-  # code goes here
+  def get_english_meaning(lib,em)
+   emojis = load_library(lib) 
+   emojis.each do |key, val|
+   if em == val[:japanese]
+    return key
+   end
+  # emotions
+  end
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(lib,em)
+  emojis = load_library(lib)
+   emojis.each do |key, val|
+   if em == val[:english]
+    return val[:japanese]
+   end
+  # emotions
+  end
+  return "Sorry, that emoticon was not found"
 end
