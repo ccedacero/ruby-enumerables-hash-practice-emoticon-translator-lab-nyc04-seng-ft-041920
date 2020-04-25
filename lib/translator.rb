059159484321b82c6172c99985fa8aa50631eafd
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-25 15:20:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,14 @@
 require "yaml"
   def load_library(lib)
    emotions = YAML.load_file(lib)
+   emoticon = {}
    emotions.each do |key, emo|
    emot[key] = {
     :english => emo[0],
     :japanese => emo[1]
   }
   end
-  return emotions
+  return emoticon
 end
 
 
